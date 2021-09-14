1347dcb0dea4a7bc8ec97ed597d8ef244fa85e1f
Tabriz Mammadov <tabrizam@code.edu.azgit config --global user.name TebrizM>
2021-09-14 23:34:18 +0400

    final versiya

---
+++ FinalVersiya/Finaltry/Enum/Position.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Finaltry.Enum
+{
+
+    // Position enum it link with Employee's Positions;
+    enum Position
+    {
+        Junior = 1,
+        Senior,
+        Manager,
+        Designer,
+        CEO
+
+    }
+}
